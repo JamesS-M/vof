8ebdf8783ba4dda969629afa82c4d0d56dfb5d7d
James <jsummerb@gmail.com>
2018-05-03 11:42:09 -0300

    Added comments and removed unused logs

--- map.js
+++ map.js
@@ -1,10 +1,15 @@
+//Loads the csv file of names
 const csvFile = '/Users/James/Documents/CS/Operatic\ Fame/Folder/Mapping\ CSVs/Mapping.csv'
 const csv=require('csvtojson')
 const fs = require('fs')
 const Json2csvParser=require('json2csv').Parser;
 
+//Declares an empty array which will hold all of the nested arrays from the csv file
 let map =[];
 
+/* A function which accepts a search (name), loops through the nested arrays from csv
+and returns the first name of the matching array (map[i][0]). If the search is '', or
+an unrecognized name, it returns undefined */
 let canonIt = (name) => {
   if (name != ''){
     for (let i = 0; i < map.length; i++) {
@@ -16,16 +21,17 @@ let canonIt = (name) => {
   }
 }
 
+//This variable is used to search through the name map
 let nameSearch = 'Benda'
 
+//Loads the csv from file, and pushes each row into the array map.
 csv()
 .fromFile(csvFile)
 .on('csv', (csvRow) => {
-  // console.log(csvRow)
   map.push(csvRow)
-
 })
+
+//After map array has been populated, we can search for a canonical name
 .on('done', () => {
-  // console.log(map)
 console.log(canonIt(nameSearch))
 })
