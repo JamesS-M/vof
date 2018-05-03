115dcebc1d29ce99e93d989ba42c7b9afa2e0435
James <jsummerb@gmail.com>
2018-05-03 12:01:16 -0300

    Changed file to test neo4j manipulation

--- test.js
+++ test.js
@@ -1,66 +1,16 @@
-const csvFilePath='/Users/James/Documents/NEO4J/Folder/Input/ReviewsTest.csv'
-const csvOutputFilePath = '/Users/James/Documents/NEO4J/Folder/Output/Reviews.csv'
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://neo4j:neo4j@localhost:11002');
 
-const csvtojson=require('csvtojson')
-const fs = require('fs')
-const Json2csvParser=require('json2csv').Parser;
-
-
-
-function extractName(manynames) {
-  let cleanNames = [];
-  let pattern = /<(.*?)>/g;
-  while(matches = pattern.exec(manynames)) {
-    cleanNames.push(matches);
-}
-  return cleanNames
-}
-
-let knownKeywords = ["Englische Oper (Gattung)",
-            "Französische Oper (Gattung)",
-            "Italienische Oper (Gattung)",
-            "Oper (Gattung)",
-          "Oper (Veranstaltung)",
-          "Oper, Libretto",
-          "Oper, Stoffe",
-          "Opera buffa",
-          "Opéra comique (Gattung)",
-          "Opera seria",
-          "Operette",
-          "Opernhäuser/haus",
-          "Opernpraxis",
-          "Opernsänger/innen",
-          "Publikum, Oper",
-          "Querelle des Bouffons",
-          "Querelle des Gluckistes et des Piccinistes",
-          "Querelle des Italiens...",
-          "Singspiel",
-          "Theatertruppen",
-          "Tragédie lyrique",
-          "Vergleich Oper/Drama"]
-
-csvtojson()
-.fromFile(csvFilePath)
-.on('json', (jsonObj, rowIndex) => { 
-  let seperatedKeyword = extractName(jsonObj['Keyword'])
-  // console.log(seperatedKeyword)
-  for (i = 0; i < seperatedKeyword.length; i++) {
-    let newArray = [];
-    newArray.push(seperatedKeyword[i][1])
-    console.log(newArray) 
+db.cypher({
+  query: 'MATCH (n:Composer) RETURN n LIMIT 25',
+  params: {}
+}, function(err, results) {
+  if (err) throw err;
+  var result = results[0];
+  if (!result) {
+    console.log('No results found.');
+  } else {
+    var Composer = result['n'];
+    console.log(JSON.stringify(Composer, null, 4))
   }
 })
-
-
-/*
-Convert jsonObj['Keywords'] to new array of keywords
-
-/*
-knownKeywords {
-  modifiedKeywords {
-    if LOOP1[i] === LOOP2[j]{
-      push LOOP2 to new array
-    }
-  }
-}
-*/
