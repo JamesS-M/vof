3c777b22c73601467e83c7fc95e044407880f69b
James <jsummerb@gmail.com>
2018-05-03 17:26:13 -0300

    Changed source filepath

--- Composers.js
+++ Composers.js
@@ -4,8 +4,8 @@ loads it as an array of JSON objects, applies modifications, and
 saves a new csv.
 */
 
-const csvFilePath='/Users/James/Documents/NEO4J/Folder/Input/Composers.csv'
-const csvOutputFilePath = '/Users/James/Documents/NEO4J/Folder/Output/Composers.csv'
+const csvFilePath='/Users/James/Documents/CS/OperaticFame/Folder/Input/csv/Composers.csv'
+const csvOutputFilePath = '/Users/James/Documents/CS/Folder/Output/Composers.csv'
 
 const csvtojson=require('csvtojson')
 const fs = require('fs')
@@ -16,31 +16,30 @@ let composersArray=[]
 // Creates json object from csv file
 csvtojson()
 .fromFile(csvFilePath)
-.on('json',(jsonObj, rowIndex)=>{
+.on('json', (jsonObj, rowIndex)=> {
     
     // Removes unused columns
-    delete jsonObj['Calendar Items']
-    delete jsonObj['Attachments']
-    delete jsonObj['Notes']
-    delete jsonObj['composer']
-    
+    // delete jsonObj['Calendar Items']
+    // delete jsonObj['Attachments']
+    // delete jsonObj['Notes']
+    // delete jsonObj['composer']
     // Pushes jsonObj into composersArray
-    composersArray.push(jsonObj)
+    composersArray.push(jsonObj['Name'])
+    console.log(jsonObj['Name'])
 
 })
 .on('done',(error)=>{
- 
     // Creates new object from Json2csvParser    
-    const fields = ['Name'];
-    const json2csvParser = new Json2csvParser({ fields });
+    // const fields = ['Name'];
+    // const json2csvParser = new Json2csvParser({ fields });
 
-    // Writes csvFile to console
-    const csvFile = json2csvParser.parse(composersArray);
-    console.log(csvFile)
+    // // Writes csvFile to console
+    // const csvFile = json2csvParser.parse(composersArray);
+    // console.log(csvFile)
     
     // Creates Composers.csv with data from csvFile
-    fs.writeFile(csvOutputFilePath, csvFile, function(err) {
-        if (err) throw err;
-        console.log('file saved');
-    })
+    // fs.writeFile(csvOutputFilePath, csvFile, function(err) {
+    //     if (err) throw err;
+    //     console.log('file saved');
+    // })
 })
