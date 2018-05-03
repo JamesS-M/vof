1805b6b414adf80f0dde55afbac17f5483840db7
James <jsummerb@gmail.com>
2018-05-03 17:27:08 -0300

    Updated with regex function

--- splitFunction.js
+++ splitFunction.js
@@ -1,18 +1,20 @@
-function splitField(field){
-	let array = []
-
-	let splitted = field.split(" ");
-	for (i=0;i<splitted.length;i++){
-		
-		let trimmed = splitted[i].substring(1, splitted[i].length-1);
-		array.push(trimmed)
-	}
-	return array
+function extractName(manynames) {
+  let cleanNames = [];
+  if (manynames.charAt(0) === '<') {
+    let pattern = /<(.*?)>/g;
+    while(matches = pattern.exec(manynames)) {
+      cleanNames.push(matches[1]);
+    }
+  } else if (manynames.charAt(0) === ''){
+  } else {
+    cleanNames.push(manynames)
+  }
+  return cleanNames
 }
 
 
 
 
-console.log(splitField("<James> <Paul> <Estelle>"))
-console.log(splitField("<one> <two three> <four>"))
+console.log(extractName("<James> <Paul> <Estelle>"))
+console.log(extractName("<one> <two three> <four>"))
 // => ["one","two three", "four"]
