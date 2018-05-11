301404698cc4c2d860b874c7e3ba2185f3b87e74
James <jsummerb@gmail.com>
2018-05-11 13:33:32 -0300

    Initial commit
    Converts date to ISO date

---
+++ redoDate.js
@@ -0,0 +1,16 @@
+let str1 = 'June 26, 1772'
+let str2 = '25 May, 2012'
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+console.log(formatDate(str2))
