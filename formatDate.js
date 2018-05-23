e6465e1495b426779a6822fce59e122ceb753381
James <jsummerb@gmail.com>
2018-05-23 12:50:26 -0300

    Initial commit
    Changes date from text to ISO

---
+++ formatDate.js
@@ -0,0 +1,23 @@
+const csvAirtable = '/Users/James/Desktop/Calendar_prefinal.csv'
+const csvtojson=require('csvtojson');
+
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+let airtableArr = [];
+
+csvtojson()
+.fromFile(csvAirtable)
+.on('csv', (csvRow) => {
+  console.log(formatDate(csvRow[7]))
+  
+})
