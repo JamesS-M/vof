b78370183bde509788a65aeb55e4ad240f48a137
JamesSM <jsummerb@gmail.com>
2018-08-20 13:37:28 -0300

    Updated

--- cypher/relationship_details.js
+++ cypher/relationship_details.js
@@ -2,7 +2,7 @@ var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "james"));
 var session = driver.session()
 
-let query = 'MATCH(n)-[r]-(m) where id(n)=139443 return keys(m), n,m,r'
+let query = 'MATCH(n)-[r]-(m) where id(n)=75 return keys(m), n,m,r'
 
 let relationships = [];
 let performed_by = 0;
@@ -18,106 +18,57 @@ let ideal_opera = 0;
 let place = 0;
 let troupe = 0;
 
-
-
 let t = new Date
 let t2
 let t3
 
-
-
+let labelMap = {};
+let relationshipMap = {};
 
 session
 .run(query)
-  .then(function (result) {
-    driver.close()
-    t2 = new Date
-    for (var i = 0; i < result.records.length; i++) {
-      //receiving data
-      relationships.push(result.records[i].toObject().r.type)
-      m_labels.push(result.records[i].toObject().m.labels.toString())
-      // console.log(result.records[i])
-    }
-    //counting relationships
-    for (var i = 0; i < relationships.length; i++) {
-      switch (relationships[i]) {
-        case 'Performed_By':
-          performed_by++
-          break;
-        case 'References':
-          references++
-          break;
-        case 'Performance_Of':
-          performance_of++
-          break;
-        case 'Performed_In':
-          performed_in++
-          break;
-        case 'Wrote':
-          wrote++
-          break;
-        default:
+.then(function (result) {
+  driver.close()
+  t2 = new Date
+
+  for (var i = 0; i < result.records.length; i++) {
+
+    let m = result.records[i].toObject().m
+    let r = []
+    r.push(result.records[i].toObject().r.type)
+
+    for (let j = 0; j < m.labels.length; j++) {
+      if (labelMap[m.labels[j]] === undefined ) {
+        labelMap[m.labels[j]] = 0
       }
-    }
-    //counting labels
-    for (var i = 0; i < m_labels.length; i++) {
-      switch (m_labels[i]) {
-        case 'Opera_Performance':
-          opera_performance++
-          break;
-        case 'Journal':
-          journal++
-          break;
-        case 'Ideal_Opera':
-          ideal_opera++
-          break;
-        case 'Place':
-          place++
-          break;
-        case 'Troupe':
-          troupe++
-          break;
-        default:
-      }
+      labelMap[m.labels[j]]++
     }
 
-    //assembling object
-    let count = {
-      'Relationships': relationships.length,
-      'Connected_Labels': m_labels.length,
-      'Performed_By': performed_by,
-      'References': references,
-      'Performance_Of': performance_of,
-      'Performed_In': performed_in,
-      'Wrote': wrote,
-      'Opera_Performance': opera_performance,
-      'Journal': journal,
-      'Ideal_Opera': ideal_opera,
-      'Place': place,
-      'Troupe': troupe
+
+    for (let k = 0; k < r.length; k++) {
+      if (relationshipMap[r[k]] === undefined ) {
+        relationshipMap[r[k]] = 0
+      }
+      relationshipMap[r[k]]++
     }
-    let object = {
-      node: 'Node Name',
-      relationships: [{
-        Performed_In: performed_in,
-        References: references,
-        Performance_Of: performance_of,
-        Performed_By: performed_by,
-        Wrote: wrote
-      }],
-      labels: [{
-        Opera_Performance: opera_performance,
-        Journal: journal,
-        Ideal_Opera: ideal_opera,
-        Place: place,
-        Troupe: troupe
-      }]
-    }
-    // console.log(count)
-    console.log(object)
-    t3 = new Date
-    console.log('Query time:')
-    console.log(t2-t)
-    console.log('Calculation time:')
-    console.log(t3-t2)
-  })  
+  }
+})
+
+.then(function (result) {
+  let object = {
+    labelMap,
+    relationshipMap
+  }
+
+  console.log(object)
+  t3 = new Date
+  console.log('Query time:')
+  console.log(t2-t)
+  console.log('Calculation time:')
+  console.log(t3-t2)
+
+})
+
+  
+
+   
