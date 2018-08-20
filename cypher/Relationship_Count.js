9f3c826660ae7734f313d3c86054fa81033c1587
JamesSM <jsummerb@gmail.com>
2018-08-20 09:46:43 -0300

    Revert "Updated file paths"
    This reverts commit e224ca5f00094d1623798bfb1f8998e9c3da51a1.

--- cypher/Relationship_Count.js
+++ cypher/Relationship_Count.js
@@ -2,77 +2,122 @@ var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "james"));
 var session = driver.session()
 
-let query = 'MATCH(n)-[r]-(m) return keys(m), n,m,r'
+let query = 'MATCH(n)-[r]-(m) where id(n)=139443 return keys(m), n,m,r'
 
-let objects = []; 
+let relationships = [];
+let performed_by = 0;
+let references = 0;
+let performance_of = 0;
+let performed_in = 0;
+let wrote = 0;
 
- 
+let m_labels = [];
+let opera_performance = 0;
+let journal = 0;
+let ideal_opera = 0;
+let place = 0;
+let troupe = 0;
+
+
+
 let t = new Date
 let t2
 let t3
 
-let properties = [];
+
+
 
 session
 .run(query)
   .then(function (result) {
     driver.close()
     t2 = new Date
-    let labelMap = {};
-    let relationshipMap = {};
-    for (var j = 0; j < result.records.length; j++) {
-      // console.log(result.records[j].toObject().n)
-    }
     for (var i = 0; i < result.records.length; i++) {
-
-      let m = result.records[i].toObject().m
-      let r = []
-      r.push(result.records[i].toObject().r.type)
-
-      for (let j = 0; j < m.labels.length; j++) {
-        if (labelMap[m.labels[j]] === undefined ) {
-          labelMap[m.labels[j]] = 0
-        }
-        labelMap[m.labels[j]]++
-      }
-
-      for (let k = 0; k < r.length; k++) {
-        if (relationshipMap[r[k]] === undefined ) {
-          relationshipMap[r[k]] = 0
-        }
-        relationshipMap[r[k]]++
+      //receiving data
+      relationships.push(result.records[i].toObject().r.type)
+      m_labels.push(result.records[i].toObject().m.labels.toString())
+      // console.log(result.records[i])
+    }
+    //counting relationships
+    for (var i = 0; i < relationships.length; i++) {
+      switch (relationships[i]) {
+        case 'Performed_By':
+          performed_by++
+          break;
+        case 'References':
+          references++
+          break;
+        case 'Performance_Of':
+          performance_of++
+          break;
+        case 'Performed_In':
+          performed_in++
+          break;
+        case 'Wrote':
+          wrote++
+          break;
+        default:
       }
     }
-    // console.log(labelMap)
-    // console.log(relationshipMap)
+    //counting labels
+    for (var i = 0; i < m_labels.length; i++) {
+      switch (m_labels[i]) {
+        case 'Opera_Performance':
+          opera_performance++
+          break;
+        case 'Journal':
+          journal++
+          break;
+        case 'Ideal_Opera':
+          ideal_opera++
+          break;
+        case 'Place':
+          place++
+          break;
+        case 'Troupe':
+          troupe++
+          break;
+        default:
+      }
+    }
 
+    //assembling object
+    let count = {
+      'Relationships': relationships.length,
+      'Connected_Labels': m_labels.length,
+      'Performed_By': performed_by,
+      'References': references,
+      'Performance_Of': performance_of,
+      'Performed_In': performed_in,
+      'Wrote': wrote,
+      'Opera_Performance': opera_performance,
+      'Journal': journal,
+      'Ideal_Opera': ideal_opera,
+      'Place': place,
+      'Troupe': troupe
+    }
     let object = {
-      labelMap,
-      relationshipMap
+      node: 'Node Name',
+      relationships: [{
+        Performed_In: performed_in,
+        References: references,
+        Performance_Of: performance_of,
+        Performed_By: performed_by,
+        Wrote: wrote
+      }],
+      labels: [{
+        Opera_Performance: opera_performance,
+        Journal: journal,
+        Ideal_Opera: ideal_opera,
+        Place: place,
+        Troupe: troupe
+      }]
     }
-
-    t3 = new Date
     // console.log(count)
-    console.log(object)    
-    console.log(`Query time: ${t2-t} ms`)
-    console.log(`Calculation time: ${t3-t2} ms`)
-  })
-
-
-
-// for (records.length) {
-//   n[i] = {
-//     labels: labelMap,
-//     relationships: relationshipMap
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
+    console.log(object)
+    t3 = new Date
+    console.log('Query time:')
+    console.log(t2-t)
+    console.log('Calculation time:')
+    console.log(t3-t2)
+  })  
