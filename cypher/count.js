991a46e9c1a1ebc0c0cf8616057fd414cee094c8
JamesSM <jsummerb@gmail.com>
2018-09-10 14:11:47 -0300

    added env variable

--- cypher/count.js
+++ cypher/count.js
@@ -1,16 +1,16 @@
-var neo4j = require('neo4j-driver').v1;
-var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "james"));
-var session = driver.session()
+const dotenv = require('dotenv').config()
 
+let connection = process.env.BOLT_CONNECTION
+let connection_user = process.env.BOLT_USER
+let connection_password = process.env.BOLT_PASSWORD
 
+const neo4j = require('neo4j-driver').v1
+const driver = neo4j.driver(connection, neo4j.auth.basic(connection_user, connection_password));
+const session = driver.session();
 
-function count() {
-  session
-  .run('Match (n) return count(n)')
-  .then(function (result) {
-    console.log('Found '+result.records[0].toObject(0)['count(n)'].low+' nodes in '+result.summary.resultAvailableAfter + 'ms')
-    driver.close()
-  })  
-}
-
-count()
+session
+.run('Match (n) return count(n)')
+.then(function (result) {
+  console.log('Found '+result.records[0].toObject(0)['count(n)'].low+' nodes in '+result.summary.resultAvailableAfter + 'ms')
+  driver.close()
+})  
